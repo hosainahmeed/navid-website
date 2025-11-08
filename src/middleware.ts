db2f2ed14d60e65b7385c3c23cbce00550df9d1a
hosainahmeed <skyfal430@gmail.com>
2025-11-08 10:05:29 +0600

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,5 +11,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/profile', '/profile/:path*',"/shop"],
+    matcher: ['/profile', '/profile/:path*'],
 };
